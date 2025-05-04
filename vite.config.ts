4ef84d119b4b028cd7748a18ed4bd4137a65b28d
Klezovich <olegolegolegoleg88@gmail.com>
2025-05-04 15:11:59 +0300

    master: fixed troubles with `npm run build`

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import Pages from 'vite-plugin-pages';
-import path from 'path';
+import * as path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
