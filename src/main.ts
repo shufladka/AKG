342992173f079e7407a3b377796b9cbb18375a7b
Klezovich <olegolegolegoleg88@gmail.com>
2025-05-04 16:51:20 +0300

    firebase: build the app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 // @ts-nocheck
 import { createApp } from 'vue';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import App from '@/App.vue';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
@@ -11,8 +13,25 @@ import '@/assets/input.css'
 const pinia = createPinia();
 pinia.use(piniaPluginPersistedstate);
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBUhPPbTN-vkPi_-8iLxR79jq4ftnQYKj0",
+  authDomain: "akg-dist.firebaseapp.com",
+  projectId: "akg-dist",
+  storageBucket: "akg-dist.firebasestorage.app",
+  messagingSenderId: "521987870165",
+  appId: "1:521987870165:web:2f2c4a15839154534d015c",
+  measurementId: "G-6SEQFY14PM"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
+
 const app = createApp(App);
 app.use(router);
 app.use(createPinia());
 app.use(logger);
+app.use(analytics)
 app.mount('#app');
