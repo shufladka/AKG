e11d2ec4b39ee27a8a2c561e4212d6af5b70727e
Klezovich <olegolegolegoleg88@gmail.com>
2025-05-04 17:00:57 +0300

    firebase: fixed contacts

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -23,34 +23,38 @@ function goToZadanie2() {
 <template>
   <div class="relative w-screen h-screen overflow-hidden">
     <div
-      class="absolute bottom-1/2 left-1/2 transform -translate-x-1/2 w-[400px] bg-white bg-opacity-90 backdrop-blur-md border border-gray-300 rounded-lg shadow-lg p-4 space-y-4 z-10"
+      class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[400px] bg-white bg-opacity-90 backdrop-blur-md border border-gray-300 rounded-lg shadow-lg p-4 space-y-4 z-10"
     >
-      <h2 class="text-lg font-semibold text-center">Ссылки на индивидуальные задания</h2>
+      <div class="flex flex-col gap-1">
+        <div class="text-md text-start">ФИО: <span class="font-semibold">Клезович Олег Сергеевич</span></div>
+        <div class="text-md text-start">Номер зачетной книжки: <span class="font-semibold">15105012</span></div>
+        <div class="text-md text-start">Группа: <span class="font-semibold">151051</span></div>
+      </div>
       <div class="flex flex-col gap-2">
         <button
           @click="goToIpr1"
           class="w-full bg-sky-600 hover:bg-sky-700 text-white py-2 rounded shadow transition"
         >
-          Перейти к ИПР1
+          Индивидуальная практическая работа №1
         </button>
         <button
           @click="goToIpr2"
           class="w-full bg-sky-600 hover:bg-sky-700 text-white py-2 rounded shadow transition"
         >
-          Перейти к ИПР2
+          Индивидуальная практическая работа №2
         </button>
 
         <button
           @click="goToZadanie1"
           class="w-full bg-sky-600 hover:bg-sky-700 text-white py-2 rounded shadow transition"
         >
-          Перейти к Заданию 1
+          Контрольная работа. Задание №1
         </button>
         <button
           @click="goToZadanie2"
           class="w-full bg-sky-600 hover:bg-sky-700 text-white py-2 rounded shadow transition"
         >
-          Перейти к Заданию 2
+          Контрольная работа. Задание №2
         </button>
       </div>
     </div>
